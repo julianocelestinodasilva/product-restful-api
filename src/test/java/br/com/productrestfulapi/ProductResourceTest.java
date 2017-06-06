4320556861a811bff0e5ab43c62203a08f3c879b
Juliano <jcelestino@raiadrogasil.com.br>
2017-06-06 17:59:58 -0300

    Estruturando o Projeto

---
+++ src/test/java/br/com/productrestfulapi/ProductResourceTest.java
@@ -0,0 +1,16 @@
+package br.com.productrestfulapi;
+
+import org.junit.Test;
+
+/**
+ * Created by juliano on 06/06/17.
+ */
+
+public class ProductResourceTest {
+
+
+    @Test
+    public void name() throws Exception {
+
+    }
+}
