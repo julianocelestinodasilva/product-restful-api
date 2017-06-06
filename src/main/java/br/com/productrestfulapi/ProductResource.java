5f6408b75d647731dce06e64b250ec3a3991b5c2
Juliano Celestino <julianocelestinodasilva@gmail.com>
2017-06-06 08:30:41 -0300

    Creating ProductResource

---
+++ src/main/java/br/com/productrestfulapi/ProductResource.java
@@ -0,0 +1,14 @@
+package br.com.productrestfulapi;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by juliano on 06/06/17.
+ */
+
+@Path("/product")
+public class ProductResource {
+
+    // http://docs.oracle.com/javaee/6/tutorial/doc/gilik.html
+
+}
