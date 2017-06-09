0e3ed13dd81f8b2171784737199ad71d31659889
Juliano Celestino <julianocelestinodasilva@gmail.com>
2017-06-09 08:35:30 -0300

    delete ProductRepository -Refactor

--- src/test/java/br/com/productrestfulapi/model/ProductRepositoryTest.java
+++ src/test/java/br/com/productrestfulapi/model/ProductRepositoryTest.java
@@ -1,14 +1,13 @@
 package br.com.productrestfulapi.model;
 
 import br.com.productrestfulapi.util.JPAUtil;
-import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
@@ -21,18 +20,22 @@ public class ProductRepositoryTest {
 
     private EntityManager em;
 
-    private Product productOne;
-    private List<Image> imagesProductOne;
+    private Product product;
+    private List<Image> imagesProduct;
+    private ProductRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository = new ProductRepository(JPAUtil.createEntityManager());
+    }
 
     @Test
     public void should_delete_product() throws Exception {
         persistProducts();
-        final long productId = productOne.getId();
-        ProductRepository repository = new ProductRepository(JPAUtil.createEntityManager());
+        final long productId = product.getId();
         repository.delete(productId);
         em = JPAUtil.createEntityManager();
         assertNull(em.find(Product.class, productId));
-
     }
 
     private void persistProducts() throws IOException {
@@ -40,18 +43,17 @@ public class ProductRepositoryTest {
         em.getTransaction().begin();
         em.createNativeQuery("DELETE FROM Image").executeUpdate();
         em.createNativeQuery("DELETE FROM Product").executeUpdate();
-        persistProductOne();
+        createProduct();
+        em.persist(product);
         em.getTransaction().commit();
         em.close();
-        // shutdown();
     }
 
-    private void persistProductOne() {
-        productOne = new Product("Primeiro Produto", "Primeiro Produto");
-        imagesProductOne = new ArrayList<Image>();
-        imagesProductOne.add(new Image(productOne));
-        imagesProductOne.add(new Image(productOne));
-        productOne.setImages(imagesProductOne);
-        em.persist(productOne);
+    private void createProduct() {
+        product = new Product("Primeiro Produto", "Primeiro Produto");
+        imagesProduct = new ArrayList<Image>();
+        imagesProduct.add(new Image(product));
+        product.setImages(imagesProduct);
     }
+
 }
